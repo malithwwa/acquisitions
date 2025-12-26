d19573eb49b36e499712a3dbab1716193278f3f3
Malith <malithwwa@gmail.com>
2025-12-26 11:42:53 +0530

    implement ESLint & Prettier

--- src/server.js
+++ src/server.js
@@ -1,8 +1,7 @@
-import app from "./app.js";
+import app from './app.js';
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server started on port http://${PORT}`);
-})
-
+  console.log(`Server started on port http://${PORT}`);
+});
